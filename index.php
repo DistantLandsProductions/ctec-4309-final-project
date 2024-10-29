75cc2700e4511933cd424e888ad7a327dd2ac599
DistantLandsProductions <113642297+DistantLandsProductions@users.noreply.github.com>
2024-10-28 20:50:37 -0500

    Project Setup

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+include("dbconn.inc.php");
+include("shared.php"); 
+
+$conn = dbConnect();
+
+?>
---
+++ shared.php
@@ -0,0 +1,27 @@
+<?php
+// store shared information in this file, such as headers, menu, and footers
+
+//HTML Header
+$HTMLHeader = 
+"<!DOCTYPE html>
+<html>
+<head>
+	<title>Unitea Bubble Tea</title>
+	<link rel='stylesheet' href='styles/style.css' type='text/css'>
+</head>
+<body>
+";
+
+
+// Product Admin Nav
+$nav = "<nav class='flexboxContainer'>
+        </nav>
+        ";
+
+//Page Footer
+$footer = "
+<footer>
+	<a href='http://cyjang.utasites.cloud/ctec4321/'>Back to the course site</a>
+</footer>
+";
+?>
