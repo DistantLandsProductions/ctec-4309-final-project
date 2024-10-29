5af9dc070897b491299d7d9cfbcfaf7ee4b351d0
DistantLandsProductions <113642297+DistantLandsProductions@users.noreply.github.com>
2024-10-28 23:34:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 include("dbconn.inc.php");
-include("shared.php"); 
+include("shared.php");
 
 $conn = dbConnect();
 
 ?>
+<?php
+print $HTMLHeader;
+print $nav;
+?>
+<main>
+
+</main>
+<?php
+print $footer;
+?>
+</body>
+
+</html>
